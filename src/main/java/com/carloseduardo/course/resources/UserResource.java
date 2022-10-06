03543d5674385a32bd297b9113817432adda9877
Carlos Eduardo <dudumonti@hotmail.com>
2022-10-06 13:27:15 -0300

    User entity and resource

---
+++ src/main/java/com/carloseduardo/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.carloseduardo.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carloseduardo.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Carlos Eduardo", "carloseduardo@gmail.com", "99999999", "123");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
